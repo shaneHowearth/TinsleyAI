1fb1b688e2fd76f45c04eada7959fca1cb929e30
Shane Howearth <shane@shanehowearth.com>
2017-06-11 19:45:28 +1000

    Creation of Monte Carlo class, and some stubbed methods
    This class will hold the methods that do the AI calculations.

---
+++ game_engine/monte_carlo.py
@@ -0,0 +1,19 @@
+class MonteCarlo(object):
+    def __init__(self, board, **kwargs):
+        # Takes an instance of a Board and, optionally, some keyword arguments.
+        # Initialises the list of game states, and the statistics tables.
+        pass
+
+    def update(self, state):
+        # Takes a game state, and appends it to the history.
+        pass
+
+    def get_play(self):
+        # Causes the AI t calculate the best move from the current game state
+        # and return it.
+        pass
+
+    def run_simulation(self):
+        # Plays out a 'random' game from the current position, then updates the
+        # statistics tables with the result.
+        pass
