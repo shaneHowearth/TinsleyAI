8eed988bfb5f364bc908703ead4ccab8cce6f45a
Shane Howearth <shane@shanehowearth.com>
2017-06-11 19:54:10 +1000

    Define length of time AI will spend searching for the next move
    A seconds argument is passed to the init to ensure the AI doesn't take forever.

--- game_engine/monte_carlo.py
+++ game_engine/monte_carlo.py
@@ -1,9 +1,14 @@
+import datetime
+
+
 class MonteCarlo(object):
     def __init__(self, board, **kwargs):
         # Takes an instance of a Board and, optionally, some keyword arguments.
         # Initialises the list of game states, and the statistics tables.
         self.board = board
         self.states = []
+        seconds = kwargs.get('time', 30)
+        self.calculation_time = datetime.timedelta(seconds=seconds)
 
     def update(self, state):
         # Takes a game state, and appends it to the history.
@@ -12,7 +17,9 @@ class MonteCarlo(object):
     def get_play(self):
         # Causes the AI t calculate the best move from the current game state
         # and return it.
-        pass
+        begin = datetime.datetime.utcnow()
+        while datetime.datetime.utcnow() - begin < self.calculation_time:
+            self.run_simulation()
 
     def run_simulation(self):
         # Plays out a 'random' game from the current position, then updates the
