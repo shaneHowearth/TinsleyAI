710944da30cc3b8f88a61336854118eeeff17c7f
Shane Howearth <shane@shanehowearth.com>
2017-06-18 17:02:47 +1000

    Create agent class

---
+++ agent.py
@@ -0,0 +1,9 @@
+"""
+This is the agent. This is the code that looks at the tree and searches for the optimim move.
+"""
+
+class agent():
+# get the tree
+# perform min_max search
+# if multiple moves meet the criteria, select one at random.
+# return optimal move
