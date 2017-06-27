c1df77c68c15b4b227dd5d1d0a7393951ff6afb4
Shane Howearth <shane@shanehowearth.com>
2017-06-27 19:21:23 +1000

    Add notes on next steps to take

--- agent.py
+++ agent.py
@@ -3,7 +3,11 @@ This is the agent. This is the code that looks at the tree and searches for the 
 """
 
 class agent():
+
 # get the tree
 # perform min_max search
 # if multiple moves meet the criteria, select one at random.
 # return optimal move
+# make move
+# wait for turn
+# start again
